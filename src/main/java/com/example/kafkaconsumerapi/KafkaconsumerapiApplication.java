3323dbab8d41fc8147607ba20ce0eac6bc94b9bb
Pawan Kumar <pawan41281@gmail.com>
2025-07-18 23:56:41 +0530

    Add files via upload

---
+++ src/main/java/com/example/kafkaconsumerapi/KafkaconsumerapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaconsumerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaconsumerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaconsumerapiApplication.class, args);
+	}
+
+}
