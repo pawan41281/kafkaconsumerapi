542a06c0366bc53cced798596388e4ceabba73f2
Pawan Kumar <pawan41281@gmail.com>
2025-07-20 14:51:38 +0530

    code update

--- src/main/java/com/example/kafkaconsumerapi/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafkaconsumerapi/consumer/KafkaConsumer.java
@@ -4,21 +4,23 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import vo.EmployeeVo;
 
 @Configuration
 @Slf4j
+@AllArgsConstructor
 public class KafkaConsumer {
 
+	private final String employeeTopic = "employee-topic";
 
-	private final String kafkaTopic = "mytopic";
-
-	@KafkaListener(topics = kafkaTopic, groupId = "group1")
+	@KafkaListener(topics = employeeTopic, groupId = "group1")
 	public void consumeJson(ConsumerRecord<String, EmployeeVo> record) {
-		log.info("Message consume successfully. Result :: topic :: {}  | key :: {} | Message :: {}", kafkaTopic, record.key(), record.value());
+		log.info("Message consume successfully. Result :: topic :: {}  | key :: {} | Message :: {}", employeeTopic,
+				record.key(), record.value());
 	}
-	
+
 //	@KafkaListener(topics = kafkaTopic, groupId = "group1")
 //	public void consumeJson(EmployeeVo EmployeeVo) {
 //		log.info("Message consume successfully. Result :: topic :: {}  | Message :: {}", kafkaTopic, EmployeeVo);
